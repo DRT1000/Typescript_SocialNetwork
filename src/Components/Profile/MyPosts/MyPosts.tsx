948bf7e18dc2b8588b6add743860b23454e723fa
Your Name <drt10000@gmail.com>
2021-01-19 18:03:11 +0300

    Ref

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -8,17 +8,21 @@ import {ProfilePageType} from "../../../Redux/state";
 
 function MyPosts(props:ProfilePageType) {
     let postElements = props.posts.map((p) => <Post message={p.message} likesCount={p.likesCount}/>)
+    let newPostElement=React.createRef<HTMLTextAreaElement>()
+    function addPost(){
+        let text=newPostElement.current?.value
+        alert(text)
+    }
     return (
         <div className={s.postsBlock}>
             <div>
                 <h3>My posts</h3>
                 <div>
                     <div>
-                        <textarea></textarea>
+                        <textarea ref={newPostElement}></textarea>
                     </div>
                     <div>
-                        <button>Add post</button>
-                        <button>Remove</button>
+                        <button onClick={addPost}>Add post</button>
                     </div>
                 </div>
             </div>
